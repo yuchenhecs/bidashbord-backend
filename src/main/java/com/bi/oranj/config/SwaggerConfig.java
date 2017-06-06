0a00398fc19c9c34c8782b74fb240326bf1479f7
Harshavardhan Patil <harshavardhanpatil@Harshavardhans-MacBook-Pro.local>
2017-06-06 17:59:01 -0500

    BI-44 changed mysql URL on dev environment

--- src/main/java/com/bi/oranj/config/SwaggerConfig.java
+++ src/main/java/com/bi/oranj/config/SwaggerConfig.java
@@ -22,7 +22,6 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.any())
-//                .paths(PathSelectors.any())
                 .paths(Predicates.not(PathSelectors.regex("/error.*")))
                 .build()
                 .apiInfo(apiInfo());
