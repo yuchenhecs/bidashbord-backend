a702d7d3275a16e95f29df563a52df4f1c85ed39
Yuchen He <yuchen@runoranj.com>
2017-06-23 11:01:57 -0500

    add: schedule daily update for net worth

--- src/main/java/com/bi/oranj/scheduler/ScheduledTasks.java
+++ src/main/java/com/bi/oranj/scheduler/ScheduledTasks.java
@@ -59,9 +59,13 @@ public class ScheduledTasks {
         oranjService.fetchPositionsData();
         log.info("Fetching 'Positions' DATA: DONE");
     }
-
-
-    // add fetch net worth
-
+    
+    @Scheduled(cron = "0 50 2 * * *")
+    public void triggerGetNetWorth (){
+        String yesterday = dateFormat.format(yesterday());
+        log.info("Fetching 'Net Worth' DATA");
+        oranjService.getNetWorth(yesterday);
+        log.info("Fetching 'Net Worth' DATA: DONE");
+    }
 
 }
