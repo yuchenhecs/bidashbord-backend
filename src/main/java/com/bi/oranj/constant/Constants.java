e807446053e77f886d9262a06fce45f47c40dc63
Harshavardhan Patil <harshavardhanpatil@Harshavardhans-MacBook-Pro.local>
2017-06-28 15:59:12 -0500

    Fixed Sonar issues

--- src/main/java/com/bi/oranj/constant/Constants.java
+++ src/main/java/com/bi/oranj/constant/Constants.java
@@ -2,6 +2,10 @@ package com.bi.oranj.constant;
 
 public class Constants {
 
+    private Constants(){
+        throw new IllegalStateException("Constants class");
+    }
+
     public static final String SUCCESS = "SUCCESS";
     public static final String START_SECOND_OF_THE_DAY = "00:00:00";
     public static final String LAST_SECOND_OF_THE_DAY = "23:59:59";
