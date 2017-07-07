aff408a739aebe04d2ca93712f0be9bc2e84dc6d
Harshavardhan Patil <harshavardhanpatil@Harshavardhans-MacBook-Pro.local>
2017-07-07 13:44:28 -0500

    BI-153 increased page size

--- src/main/java/com/bi/oranj/model/bi/BaseLoginMetrics.java
+++ src/main/java/com/bi/oranj/model/bi/BaseLoginMetrics.java
@@ -3,9 +3,6 @@ package com.bi.oranj.model.bi;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
-import java.math.BigDecimal;
-import java.math.BigInteger;
-
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class BaseLoginMetrics {
