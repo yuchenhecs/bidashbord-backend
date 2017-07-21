c5d51ebffc6afd9c9e82aae036812815d5d1f26a
Jaloliddin Bakirov <jaloliddinbakirov@Jaloliddins-MacBook-Pro.local>
2017-07-21 10:21:50 -0500

    BI-186 changed query

--- src/main/resources/db/migration/V7__gamification_tables_and_procedures.sql
+++ src/main/resources/db/migration/V7__gamification_tables_and_procedures.sql
@@ -48,8 +48,8 @@ CREATE PROCEDURE compute_category_values (date1 varchar(15), date2 varchar(15), 
     BEGIN
     insert into gamification_categories (advisor_id, aum, net_worth, hni, conversion_rate, avg_conversion_time, retention_rate, weekly_logins, aum_growth, net_worth_growth,
              clientele_growth)
-    SELECT id advisor_id, weekly_logins.weekly_logins, aum.amount aum, net_worth.amount net_worth, hni.amount hni, cr.conversion_rate,
-		avg_conv_time.avg_conversion_time, rr.retention_rate, TRUNCATE(qoq_aum,3) qoq_aum,
+SELECT id advisor_id, aum.amount aum, net_worth.amount net_worth, hni.amount hni, cr.conversion_rate,
+		avg_conv_time.avg_conversion_time, rr.retention_rate, weekly_logins.weekly_logins, TRUNCATE(qoq_aum,3) qoq_aum,
 		TRUNCATE(qoq_networth, 3) qoq_networth, TRUNCATE(clientele_growth, 3) clientele_growth from advisors a left join
 	(
 		select l.advisor_id, l.logins weekly_logins from (
@@ -57,7 +57,7 @@ CREATE PROCEDURE compute_category_values (date1 varchar(15), date2 varchar(15), 
 					(select id, advisor_id from clients where active = 1) c on a.id = c.advisor_id left join
 					(select client_id, count(client_id) count from analytics
 											where role_id = 6
-													and session_start_date between week_ago and today group by client_id) a
+													and session_start_date between "2017-07-04" and "2017-07-11" group by client_id) a
 					on c.id = a.client_id group by a.id
 					) l
 	) weekly_logins on a.id = weekly_logins.advisor_id left join
@@ -68,7 +68,7 @@ CREATE PROCEDURE compute_category_values (date1 varchar(15), date2 varchar(15), 
 				left join
 		    	(SELECT p.client_id, p.amount
 		    	from positions p
-		        where date(p.position_updated_on) IN (today) ) aum
+		        where date(p.position_updated_on) IN ("2017-07-11") ) aum
 		        on aum.client_id = c.id
 		        group by a.id
 		        ) aum
@@ -77,7 +77,7 @@ CREATE PROCEDURE compute_category_values (date1 varchar(15), date2 varchar(15), 
 		select net.advisor_id, net.amount from (
 				SELECT a.id advisor_id, @net_cal := IFNULL(sum(net.amount), 0) amount from advisors a left join
 				(SELECT id, advisor_id from clients where active = 1) c on c.advisor_id = a.id left join
-				(SELECT net.client_id, net.value amount from networth net where date(net.date) in (today)) net
+				(SELECT net.client_id, net.value amount from networth net where date(net.date) in ("2017-07-11")) net
 				on net.client_id = c.id
 				group by a.id
 				) net
@@ -86,7 +86,7 @@ CREATE PROCEDURE compute_category_values (date1 varchar(15), date2 varchar(15), 
 		SELECT hni.advisor_id, hni.amount from (
 				SELECT a.id advisor_id, @hni_cal := IFNULL(count(hni.client_id), 0) amount from advisors a left join
         		(SELECT id, advisor_id from clients where active = 1) c on c.advisor_id = a.id left join
-        		(SELECT client_id from networth where value > benchmark and date in (today)) hni
+        		(SELECT client_id from networth where value > 1000000 and date in ("2017-07-11")) hni
         		on c.id = hni.client_id
         		group by a.id) hni
 	) hni on a.id = hni.advisor_id left join
@@ -115,7 +115,7 @@ CREATE PROCEDURE compute_category_values (date1 varchar(15), date2 varchar(15), 
 							SELECT a.id advisor_id, @aum_cal1 := IFNULL(sum(aum1.amount), 0) amount from advisors a left join
 			            	(SELECT id, advisor_id from clients where active = 1) c on c.advisor_id = a.id
 							left join
-			    			(SELECT p.client_id, p.amount from positions p where date(p.position_updated_on) IN (date1) ) aum1
+			    			(SELECT p.client_id, p.amount from positions p where date(p.position_updated_on) IN ("2017-01-01") ) aum1
 					    	on aum1.client_id = c.id
 					    	group by a.id
 						) aum1 left join
@@ -123,7 +123,7 @@ CREATE PROCEDURE compute_category_values (date1 varchar(15), date2 varchar(15), 
 					        SELECT a.id advisor_id, @aum_cal2 := IFNULL(sum(aum2.amount), 0) amount from advisors a left join
 			        	    (SELECT id, advisor_id from clients where active = 1) c on c.advisor_id = a.id
 							left join
-			    			(SELECT p.client_id, p.amount from positions p where date(p.position_updated_on) IN (date2) ) aum2
+			    			(SELECT p.client_id, p.amount from positions p where date(p.position_updated_on) IN ("2017-04-01") ) aum2
 					    	on aum2.client_id = c.id
 					    	group by a.id
 						) aum2 on aum1.advisor_id = aum2.advisor_id left join
@@ -131,7 +131,7 @@ CREATE PROCEDURE compute_category_values (date1 varchar(15), date2 varchar(15), 
 					        SELECT a.id advisor_id, @aum_cal3 := IFNULL(sum(aum3.amount), 0) amount from advisors a left join
 			        	    (SELECT id, advisor_id from clients where active = 1) c on c.advisor_id = a.id
 							left join
-			    			(SELECT p.client_id, p.amount from positions p where date(p.position_updated_on) IN (date3) ) aum3
+			    			(SELECT p.client_id, p.amount from positions p where date(p.position_updated_on) IN ("2017-07-01") ) aum3
 					    	on aum3.client_id = c.id
 					    	group by a.id
 						) aum3 on aum1.advisor_id = aum3.advisor_id left join
@@ -139,7 +139,7 @@ CREATE PROCEDURE compute_category_values (date1 varchar(15), date2 varchar(15), 
 					        SELECT a.id advisor_id, @aum_cal4 := IFNULL(sum(aum4.amount), 0) amount from advisors a left join
 			        	    (SELECT id, advisor_id from clients where active = 1) c on c.advisor_id = a.id
 							left join
-			    			(SELECT p.client_id, p.amount from positions p where date(p.position_updated_on) IN (date4) ) aum4
+			    			(SELECT p.client_id, p.amount from positions p where date(p.position_updated_on) IN ("2017-10-01") ) aum4
 					    	on aum4.client_id = c.id
 					    	group by a.id
 						) aum4 on aum1.advisor_id = aum4.advisor_id left join
@@ -147,7 +147,7 @@ CREATE PROCEDURE compute_category_values (date1 varchar(15), date2 varchar(15), 
 					        SELECT a.id advisor_id, @aum_cal5 := IFNULL(sum(aum5.amount), 0) amount from advisors a left join
 			        	    (SELECT id, advisor_id from clients where active = 1) c on c.advisor_id = a.id
 							left join
-			    			(SELECT p.client_id, p.amount from positions p where date(p.position_updated_on) IN (today) ) aum5
+			    			(SELECT p.client_id, p.amount from positions p where date(p.position_updated_on) IN ("2017-01-15") ) aum5
 					    	on aum5.client_id = c.id
 					    	group by a.id
 						) aum5 on aum1.advisor_id = aum5.advisor_id
@@ -161,14 +161,14 @@ CREATE PROCEDURE compute_category_values (date1 varchar(15), date2 varchar(15), 
 							(
 			            		SELECT a.id advisor_id, @net_cal1 := IFNULL(sum(net1.val), 0) amount from advisors a left join
 				            	(SELECT id, advisor_id from clients where active = 1) c on c.advisor_id = a.id left join
-					            (SELECT client_id, value val from networth net where date(date) in (date1)) net1
+					            (SELECT client_id, value val from networth net where date(date) in ("2017-01-01")) net1
 					            on c.id = net1.client_id
 					            group by a.id
 				            ) net1 left join
 				            (
 				            	SELECT a.id advisor_id, @net_cal2 := IFNULL(sum(net2.val), 0) amount from advisors a left join
 				            	(SELECT id, advisor_id from clients where active = 1) c on c.advisor_id = a.id left join
-					            (SELECT client_id, value val from networth net where date(date) in (date2)) net2
+					            (SELECT client_id, value val from networth net where date(date) in ("2017-04-01")) net2
 					            on c.id = net2.client_id
 					            group by a.id
 				            ) net2 on net1.advisor_id = net2.advisor_id
@@ -176,7 +176,7 @@ CREATE PROCEDURE compute_category_values (date1 varchar(15), date2 varchar(15), 
 				            (
 				            	SELECT a.id advisor_id, @net_cal3 := IFNULL(sum(net3.val), 0) amount from advisors a left join
 				            	(SELECT id, advisor_id from clients where active = 1) c on c.advisor_id = a.id left join
-					            (SELECT client_id, value val from networth net where date(date) in (date3)) net3
+					            (SELECT client_id, value val from networth net where date(date) in ("2017-07-01")) net3
 					            on c.id = net3.client_id
 					            group by a.id
 				            ) net3 on net1.advisor_id = net3.advisor_id
@@ -184,7 +184,7 @@ CREATE PROCEDURE compute_category_values (date1 varchar(15), date2 varchar(15), 
 				            (
 				            	SELECT a.id advisor_id, @net_cal4 := IFNULL(sum(net4.val), 0) amount from advisors a left join
 				            	(SELECT id, advisor_id from clients where active = 1) c on c.advisor_id = a.id left join
-					            (SELECT client_id, value val from networth net where date(date) in (date4)) net4
+					            (SELECT client_id, value val from networth net where date(date) in ("2017-10-01")) net4
 					            on c.id = net4.client_id
 					            group by a.id
 				            ) net4 on net1.advisor_id = net4.advisor_id
@@ -192,7 +192,7 @@ CREATE PROCEDURE compute_category_values (date1 varchar(15), date2 varchar(15), 
 				            (
 				            	SELECT a.id advisor_id, @net_cal5 := IFNULL(sum(net5.val), 0) amount from advisors a left join
 				            	(SELECT id, advisor_id from clients where active = 1) c on c.advisor_id = a.id left join
-					            (SELECT client_id, value val from networth net where date(date) in (today)) net5
+					            (SELECT client_id, value val from networth net where date(date) in ("2017-01-15")) net5
 					            on c.id = net5.client_id
 					            group by a.id
 				            ) net5 on net1.advisor_id = net5.advisor_id
@@ -204,19 +204,19 @@ CREATE PROCEDURE compute_category_values (date1 varchar(15), date2 varchar(15), 
 				 (1 + IFNULL((t.count4 - t.count3)/NULLIF(t.count3, 0), 0)) * (1 + IFNULL((t.count5 - t.count4)/NULLIF(t.count4, 0), 0)) - 1) * 100 as clientele_growth from (
 				select a.id, c1.count1, c2.count2, c3.count3, c4.count4, c5.count5 from advisors a
 				left join
-				(select count(id) count1, advisor_id from clients where client_created_on < date1 and active = 1 and role_id = 6 group by advisor_id) c1
+				(select count(id) count1, advisor_id from clients where client_created_on < "2017-01-01" and active = 1 and role_id = 6 group by advisor_id) c1
 				on a.id = c1.advisor_id
 				left join
-				(select count(id) count2, advisor_id from clients where client_created_on < date2 and active = 1 and role_id = 6 group by advisor_id) c2
+				(select count(id) count2, advisor_id from clients where client_created_on < "2017-04-01" and active = 1 and role_id = 6 group by advisor_id) c2
 				on a.id = c2.advisor_id
 				left join
-				(select count(id) count3, advisor_id from clients where client_created_on < date3 and active = 1 and role_id = 6 group by advisor_id) c3
+				(select count(id) count3, advisor_id from clients where client_created_on < "2017-07-01" and active = 1 and role_id = 6 group by advisor_id) c3
 				on a.id = c3.advisor_id
 				left join
-				(select count(id) count4, advisor_id from clients where client_created_on < date4 and active = 1 and role_id = 6 group by advisor_id) c4
+				(select count(id) count4, advisor_id from clients where client_created_on < "2017-10-01" and active = 1 and role_id = 6 group by advisor_id) c4
 				on a.id = c4.advisor_id
 				left join
-				(select count(id) count5, advisor_id from clients where client_created_on < today and active = 1 and role_id = 6 group by advisor_id) c5
+				(select count(id) count5, advisor_id from clients where client_created_on < "2017-01-15" and active = 1 and role_id = 6 group by advisor_id) c5
 				on a.id = c5.advisor_id
 				group by a.id, c1.count1, c2.count2, c3.count3, c4.count4, c5.count5) t
 	) clientele_growth on a.id = clientele_growth.advisor_id;
